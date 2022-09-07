40899ca3e5dfcc73a1582b6797b8b3935ec372af
Lokesh Sharma <104984979+lokeshsixberries@users.noreply.github.com>
2022-09-07 21:40:01 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,8 @@
-import { useState, useEffect } from "react";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  const [isAuth, setIsAuth] = useState(false);
-  useEffect(() => {
-    if (window.location.pathname === "/") {
-      const userInput = window.prompt("Enter your Secrect Key");
-      if (userInput === "test") {
-        setIsAuth(true);
-      } else {
-        window.close();
-      }
-    } else {
-      setIsAuth(true);
-    }
-  }, []);
 
-  return <> {isAuth && <Component {...pageProps} />}</>;
+  return(<Component {...pageProps} />);
 }
 
 export default MyApp;
